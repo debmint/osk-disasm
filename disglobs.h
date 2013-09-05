9bdd353a2e8216f2f8bc998f4bad6cb4721a8428
debmint <debmint@user.noreply.github.com>
2013-09-05 15:31:47 +0000

    The program will now read the Module Header

--- disglobs.h
+++ disglobs.h
@@ -26,12 +26,12 @@ enum {
 
 xt int Pass;    /* The disassembler is a two-pass assembler */
 xt char *ModFile;   /* The module file to read */
-FILE *MFile;
+xt FILE *ModFP;
 
 /* Module header variables */
-int M_ID, M_Rev, M_Owner, M_SysRev, M_Size, M_Name,
-    M_Accs, M_Type, M_Lang, M_Attr, M_Revs, M_Edit,
-    M_Usage, M_Symbol, M_Parity,
+xt int M_ID, M_SysRev, M_Size, M_Owner, M_Name, M_Accs;
+xt char M_Type, M_Lang, M_Attr, M_Revs;
+xt int M_Edit, M_Usage, M_Symbol, M_Parity,
     M_Exec, M_Except, M_Stack, M_IData,
     M_IRefs, M_Init, M_Term, M_Port, M_Vector, M_IRQLvl,
     M_Prior, M_Mode, M_FMgr, M_PDev, M_DevCon, M_Opt,
@@ -41,5 +41,5 @@ int M_ID, M_Rev, M_Owner, M_SysRev, M_Size, M_Name,
     M_SysPri, M_MinPri, M_MaxAge, M_MDirSiz, M_Events, M_Compat,
     M_Compat2, M_MemLst, M_IRQStk, M_ColdTrys, M_Id12;
 
-int HdrEnd;   /* The first byte past end of header, usefule for begin of Pass 2 */
-int ModType;   /* The type of module */
+xt int HdrEnd;   /* The first byte past end of header, usefule for begin of Pass 2 */
+xt int ModType;   /* The type of module */
